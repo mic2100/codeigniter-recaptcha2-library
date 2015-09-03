4c14a2110d0bad1ffa57d8a862ea11f2f7b01723
Michael Bardsley <me@mic-b.co.uk>
2015-09-03 19:43:55 +0100

    remove some comments and removed some unused vars

--- libraries/Recaptcha2.php
+++ libraries/Recaptcha2.php
@@ -3,17 +3,14 @@
 class Recaptcha2
 {
     private $ci;
-    private $siteKey;
     private $secretKey;
     private $url;
 
     public function __construct()
     {
         $this->ci =& get_instance();
-
         $this->config->load('recaptcha2');
 
-        $this->siteKey = $this->ci->config->item('recaptcha2-site-key');
         $this->secretKey = $this->ci->config->item('recaptcha2-secret-key');
         $this->url = $this->ci->config->item('recaptcha2-verify-url');
     }
@@ -39,16 +36,15 @@ class Recaptcha2
             'remoteIp' => $this->ci->input->ip_address(),
         );
 
-        // Get cURL resource
         $curl = curl_init();
-        // Set some options - we are passing in a useragent too here
-        curl_setopt_array($curl, array(
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => $this->url . '?' . http_build_query($parameters),
-        ));
-        // Send the request & save response to $resp
+        curl_setopt_array(
+            $curl,
+            array(
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_URL => $this->url . '?' . http_build_query($parameters),
+            )
+        );
         $response = curl_exec($curl);
-        // Close request to clear up some resources
         curl_close($curl);
 
         return $response;
